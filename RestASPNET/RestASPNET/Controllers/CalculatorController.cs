355e1adcf95f639dfa6b0a0ed8e78daf74d790fe
tecjeferson <tecjeferson@gmail.com>
2019-08-13 22:30:10 -0300

    It is only getting the secondNumber, check the error on firstNumber

---
+++ RestASPNET/RestASPNET/Controllers/CalculatorController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestASPNET.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : Controller
+    {
+         // GET api/values/5
+        [HttpGet("{fisrtNumber}/{secondNumber}")]
+        public  IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+         
+            return BadRequest("Invalid Input!!!");
+
+
+        }
+
+        private decimal ConvertToDecimal(string numberV)
+        {
+            decimal decimalValue;
+            if (decimal.TryParse(numberV, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+     
+            bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+    }
+}
