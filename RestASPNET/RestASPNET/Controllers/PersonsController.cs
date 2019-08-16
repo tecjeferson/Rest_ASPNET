6a1f379e38fb8f36f92abbdca7a7a2bc4c85d38b
tecjeferson <tecjeferson@gmail.com>
2019-08-16 08:32:54 -0300

    Implemented Person.cs in model

--- RestASPNET/RestASPNET/Controllers/PersonsController.cs
+++ RestASPNET/RestASPNET/Controllers/PersonsController.cs
@@ -8,7 +8,7 @@ namespace RestASPNET.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class PersonsController : ControllerBase
     {
         // GET api/values
         [HttpGet]
